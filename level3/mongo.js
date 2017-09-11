1fe691a59ae951c1ec9435a8f57c34965bb6840b
Edward Dupre <edwd42@gmail.com>
2017-09-11 20:23:43 +0000

    complete homework Mongo Level 3

--- level3/mongo.js
+++ level3/mongo.js
@@ -34,5 +34,73 @@ Write the command to sort the collection by the monsters level with the highest 
 
 
 var mongo = function(db){
+    //Write the command that will find all monsters with an attack lower than 10
     db.monsters.find({"stats.attack":{"$lt":10}})
+    
+    // Write the command that will find all monsters with a level above 5 but below 15 inclusive
+    db.monsters.find({"stats.attack":{"$lte":15, "$gte":5}})
+    
+    // Write the command to find monsters that do not have the “bite” attack.
+    db.monsters.find({"attacks":{"$ne":"bite"}})
+    
+    // Write the command to find monsters with levels less than 6 but only return the name, level and health attributes
+    db.monsters.find({"Level":{"$lt":6}},{"name":true,"Level":true,"health":true,"_id":false})
+    
+    // Write the command to find all monsters who attack is between 10 and 20 but do not include the monsters health or style
+    db.monsters.find({"stats.attack":{"$gt":10, "$lt":20}},{"health":false,"style":false})
+    
+    // Write the command to find out how many monsters are in the collection.
+    db.monsters.find().count()
+    
+    // Write the command to sort the collection by the monsters level with the highest level at the top and the lowest at the bottom
+    db.monsters.find().sort({"Level":-1})
 };
+
+
+db.monsters.insert(
+    {
+    	"name": "Cat",
+    	"Level": 42,
+    	"health": 9,
+    	"type": "Kitty",
+    	"attacks": ["scratch", "bite", "hiss"],
+    	"stats": {"attack": 9, "defense": 2},
+    	"style": "smelly"
+    }
+)
+
+db.monsters.insert(
+    {
+    	"name": "Rat",
+    	"Level": 4,
+    	"health": 1,
+    	"type": "Rhodent",
+    	"attacks": ["bite", "jump", "scare"],
+    	"stats": {"attack": 8, "defense": 3},
+    	"style": "ugly"
+    }
+)
+
+db.monsters.insert(
+    {
+    	"name": "Ghost",
+    	"Level": 12,
+    	"health": 0,
+    	"type": "spooky",
+    	"attacks": ["shake", "rattle", "roll"],
+    	"stats": {"attack": 4, "defense": 2},
+    	"style": "invisible"
+    }
+)
+
+db.monsters.insert(
+    {
+    	"name": "Dragon",
+    	"Level": 120,
+    	"health": 10,
+    	"type": "spooky",
+    	"attacks": ["fire", "fly", "tail"],
+    	"stats": {"attack": 14, "defense": 22},
+    	"style": "mean"
+    }
+)
